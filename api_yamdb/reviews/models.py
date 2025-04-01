318dae6c8dd8e5323b7ff514add5dc213cbfdbcb
Evgeny Klyukin <key.klyukin@gmail.com>
2025-04-01 22:05:27 +0500

    feat: add reviews model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Reviews(models.Model):
+    # Заглушка, планируется ForeignKey с Title
+    title_id = models.IntegerField()
+    text = models.TextField(verbose_name="Текст отзыва")
+    author = models.PositiveIntegerField(verbose_name="ID автора")
+    # Заглушка, реализация рейтинга после создания всех моделей
+    score = models.IntegerField()
+    pub_date = models.DateTimeField(verbose_name="Дата публикации")
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return f"Отзыв {self.id} на {self.title.name} ({self.score}/10)"
