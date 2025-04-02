e4ebefcc70967e451cb6fe4d484071a20b1ac95b
Evgeny Klyukin <key.klyukin@gmail.com>
2025-04-02 06:04:32 +0500

    feat: add model and admin panel for reviews

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import Reviews
+
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'score', 'pub_date')
+    list_filter = ('score', 'pub_date')
+    search_fields = ('text', 'author__username', 'title__name')
+    readonly_fields = ('pub_date',)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,19 +1,41 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
+User = get_user_model()
 
 
 class Reviews(models.Model):
     # Заглушка, планируется ForeignKey с Title
     title_id = models.IntegerField()
     text = models.TextField(verbose_name="Текст отзыва")
-    author = models.PositiveIntegerField(verbose_name="ID автора")
-    # Заглушка, реализация рейтинга после создания всех моделей
-    score = models.IntegerField()
-    pub_date = models.DateTimeField(verbose_name="Дата публикации")
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор отзыва'
+    )
+    score = models.PositiveSmallIntegerField(
+        verbose_name='Оценка',
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    pub_date = models.DateTimeField(
+        verbose_name="Дата публикации",
+        auto_now_add=True
+    )
 
     class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'],
+                name='unique_review',
+                message='Вы уже оставляли отзыв на это произведение'
+            )
+        ]
         verbose_name = 'Отзыв'
         verbose_name_plural = 'Отзывы'
         ordering = ['-pub_date']
 
     def __str__(self):
-        return f"Отзыв {self.id} на {self.title.name} ({self.score}/10)"
+        return (f"Отзыв {self.author.username} на {self.title.name} "
+                f"({self.score}/10)")
