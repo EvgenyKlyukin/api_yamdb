414c69c75b22373b2cce2235bb54eb95429b8a70
Evgeny Klyukin <key.klyukin@gmail.com>
2025-04-02 06:32:07 +0500

    feat: add model and admin panel for comments

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.constants import TEXT_RESTRICTION
+from reviews.models import Comments
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('review', 'author', 'pub_date', 'short_text')
+    list_filter = ('pub_date', 'author')
+    search_fields = ('text', 'author__username', 'review__text')
+    readonly_fields = ('pub_date',)
+
+    def short_text(self, obj):
+        if len(obj.text) > TEXT_RESTRICTION:
+            obj.text[:TEXT_RESTRICTION] + '...'
+        else:
+            obj.text
+    short_text.short_description = 'Текст'
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Comments(models.Model):
+    review_id = models.ForeignKey(
+        'Review',
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв'
+    )
+    text = models.TextField(verbose_name='Текст комментария')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор комментария'
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['pub_date']
+
+    def __str__(self):
+        return f'Комментарий {self.author.username} к отзыву {self.review_id}'
