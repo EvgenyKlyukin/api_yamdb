a3b7351c384172c678ab84a35cc4eb97df260ca9
MrFadzay <janwise94@gmail.com>
2025-04-05 13:13:09 +0400

    feat: implement user management with registration, token generation, and permissions

---
+++ api_yamdb/users/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'role',
+        'is_staff'
+    )
+    search_fields = ('username', 'email')
+    list_filter = ('role',)
+    empty_value_display = '-пусто-'
+    list_editable = ('role',)
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {
+            'fields': ('first_name', 'last_name', 'email', 'bio')
+        }),
+        ('Permissions', {
+            'fields': (
+                'role',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions'
+            ),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
